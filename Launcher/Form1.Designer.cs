e5d516ae745202b7ae873adf4a40c041fdb67f9d
Omie <preachandteach@outlook.com>
2022-10-24 13:53:32 -0700

    Update Form1.Designer.cs

--- Launcher/Form1.Designer.cs
+++ Launcher/Form1.Designer.cs
@@ -57,7 +57,7 @@
             // updater1
             // 
             //SET TO THE UPDATEINFO.DAT FILE ON YOUR SERVER HOST
-            this.updater1.UpdateUrl = "http://thecrystalrefuge.com/Updates/UpdateInfo.dat";
+            this.updater1.UpdateUrl = "http://website.com/Updates/UpdateInfo.dat";
             // 
             // button2
             // 
